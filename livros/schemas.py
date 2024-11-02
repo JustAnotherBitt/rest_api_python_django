c8411379dceea3060af4ea13ad15a10ae1f204de
LeRodrigues2005 <leticiadepatta@gmail.com>
2024-11-02 11:29:46 -0300

    first commit

---
+++ livros/schemas.py
@@ -0,0 +1,20 @@
+from ninja import ModelSchema, Schema
+from .models import Livros
+
+
+class LivroSchema(ModelSchema):
+    class Meta:
+        model = Livros
+        fields = ['nome', 'streaming', 'categorias']
+
+
+class AvaliacaoSchema(ModelSchema):
+    class Meta:
+        model = Livros
+        fields = ['nota', 'comentarios']
+
+
+class FiltrosSortear(Schema):
+    nota_minima: int = None
+    categorias: int = None
+    reassistir: bool = False
